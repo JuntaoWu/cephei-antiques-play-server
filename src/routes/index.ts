1683bbb5e5ef8aad6dc9c9b69afc39a90998f598
Tony Juntao. Wu <wujuntaocn@outlook.com>
2018-12-13 17:29:15 +0800

    router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,8 @@ import userRouter from './user';
 
 import playerinfoRouter from './playerinfo';
 
+import sharedRouter from './shared';
+
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -13,5 +15,7 @@ import playerinfoRouter from './playerinfo';
 router.use('/users', userRouter);
 
 router.use('/playerinfo', playerinfoRouter);
+
+router.use('/shared', sharedRouter);
 
 export default router;
